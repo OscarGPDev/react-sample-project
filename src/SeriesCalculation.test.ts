c3bfdc4792af05f118218998da4617aeb4b3043d
Oscar Guzman <oscarprograb@gmail.com>
2022-09-04 12:15:06 -0500

    Progress update: Unit tests finished.

--- src/SeriesCalculation.test.ts
+++ src/SeriesCalculation.test.ts
@@ -13,3 +13,31 @@ test('f(3)', () => {
     const seriesCalculation = new SeriesCalculation();
     expect(seriesCalculation.calcSeriesTerm(3)).toEqual(84);
 });
+test('f(4)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(4)).toEqual(756);
+});
+test('f(5)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(5)).toEqual(3780);
+});
+test('f(6)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(6)).toEqual(14700);
+});
+test('f(7)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(7)).toEqual(55440);
+});
+test('f(8)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(8)).toEqual(149058);
+});
+test('f(9)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(9)).toEqual(419832);
+});
+test('f(10)', () => {
+    const seriesCalculation = new SeriesCalculation();
+    expect(seriesCalculation.calcSeriesTerm(10)).toEqual(976752);
+});
